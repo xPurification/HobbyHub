ef2ae68c8fe38ad0525cad9499b28b5d2c16191d
xPurification <136507977+xPurification@users.noreply.github.com>
2025-03-17 16:39:36 -0400

    rehost

--- client/src/client.js
+++ client/src/client.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const URL = 'https://gtfqkqzwsptvafctpmly.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0ZnFrcXp3c3B0dmFmY3RwbWx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI2ODQ3MjUsImV4cCI6MjAyODI2MDcyNX0.2VvM0KhQgvi8Dlf5CQSsaapJkcHkbYi0AQOX7VmY4RI';
+const URL = 'https://ahvrxniwhbxlhualkzgt.supabase.co';
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFodnJ4bml3aGJ4bGh1YWxremd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyNDE3MDYsImV4cCI6MjA1NzgxNzcwNn0.Douk2itMQ0GnfbUG1ZTX5bvaqvzDQEUheEjm38MF-FA';
 
 export const supabase = createClient(URL, API_KEY);
 
